7460f7640ed216be8ba18faa3d2863c0e4f543c9
cpw1205 <cpw1205@gmail.com>
2026-08-26 10:42:01 +0900

    Update route.ts

--- app/api/applications/route.ts
+++ app/api/applications/route.ts
@@ -4,7 +4,7 @@ import { supabase } from "@/lib/supabase";
 export async function GET(req: Request) {
   const password = req.headers.get("x-admin-password");
 
-  if (password !== process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
+  if (password !== process.env.ADMIN_PASSWORD) 
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
